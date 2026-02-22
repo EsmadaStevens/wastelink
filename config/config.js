db8ac6853d71f3582ceb77057c358d49ba46769d
Shakiroh55 <fhatshakirat@gmail.com>
2026-02-22 11:08:40 +0100

    new changes made

--- config/config.js
+++ config/config.js
@@ -16,10 +16,17 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || "mysql"
-  }
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: process.env.DB_PORT || 3306,
+  logging: false,
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production'
+      ? { require: true, rejectUnauthorized: false }
+      : false,
+  },
+},
 };
