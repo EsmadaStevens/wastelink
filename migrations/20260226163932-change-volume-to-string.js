90344a1461d9437d44cd651e9349a463e2237849
Shakiroh55 <fhatshakirat@gmail.com>
2026-02-26 17:47:04 +0100

    edit one of wastelog column

---
+++ migrations/20260226163932-change-volume-to-string.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.changeColumn("WasteLogs", "volume", {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.changeColumn("WasteLogs", "volume", {
+      type: Sequelize.ENUM("small", "medium", "large", "extra_large"),
+      allowNull: false,
+    });
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
