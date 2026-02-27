511b65ae131638076041e5d1a496f2a18dd707a0
Shakiroh55 <fhatshakirat@gmail.com>
2026-02-27 20:46:16 +0100

    added accept pickup

--- src/pickupRequest/routes/pickupRequestRoutes.js
+++ src/pickupRequest/routes/pickupRequestRoutes.js
@@ -4,7 +4,7 @@ const { authenticate } = require("../../middlewares/authMiddleware");
 const {getAvailableJobs, acceptJob, requestPickup} = require("../../pickupRequest/controllers/pickupRequestController");
 
 router.get("/available-waste", authenticate, getAvailableJobs);
-// router.patch("/accept-pickup/:id", authenticate, acceptJob);
+router.patch("/accept-pickup/:id", authenticate, acceptJob);
 router.post("/wastelog/:id/request-pickup", authenticate, requestPickup);
 router.get("/collector/jobs", authenticate, getAvailableJobs);
 router.post("/collector/jobs/:id/accept", authenticate, acceptJob);
